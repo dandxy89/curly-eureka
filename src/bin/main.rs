a23072b61554b3000892663564db82ec73f917c6
dandxy89 <dan@functorml.co.uk>
2026-01-07 19:18:37 +0000

    chore: Adding TODOs for work
    Undediced if I'll use PG or Sqlite

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,27 @@
-use axum::{Router, routing::get};
+use std::time::Duration;
+
+use axum::{Router, http::StatusCode, routing::get};
 use renewable_ts_axum::{logger::init_logging, shutdown::shutdown_signal};
 use tokio::net::TcpListener;
+use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
 use tracing::info;
 
 #[tokio::main]
 async fn main() {
-    // Initialise Logging using the Tracing crate
     init_logging();
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // TODO: Config / EnvVars?
+    // TODO: Migrations
+    // TODO: Truncate or Handle Conflicts (on load of CSV)
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        // TODO: .route()  - History Endpoint
+        // TODO: .route()  - Query Endpoint
+        .layer((
+            TraceLayer::new_for_http(),
+            TimeoutLayer::with_status_code(StatusCode::GATEWAY_TIMEOUT, Duration::from_secs(10)),
+        ));
     let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
 
     info!("Starting Axum Server...");
