d36cba66477c589a2acf70a590c721d80a73b6c0
dandxy89 <dan@functorml.co.uk>
2026-01-07 19:04:33 +0000

    feat: Adding init logging fn + graceful shutdown logic

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,19 @@
 use axum::{Router, routing::get};
+use renewable_ts_axum::{logger::init_logging, shutdown::shutdown_signal};
 use tokio::net::TcpListener;
-use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _};
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
     // Initialise Logging using the Tracing crate
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    init_logging();
 
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
     let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+
+    info!("Starting Axum Server...");
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
---
+++ src/shutdown.rs
@@ -0,0 +1,31 @@
+use std::time::Duration;
+
+use tokio::signal;
+use tracing::info;
+
+pub async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    info!("Received termination signal shutting down");
+    tokio::time::sleep(Duration::from_secs(2)).await
+}
