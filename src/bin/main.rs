902fc4ae78da89a8439fd08dc04e9c4b4bb9a5ff
dandxy89 <dan@functorml.co.uk>
2026-01-09 15:15:10 +0000

    feat: Adding query history route

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,10 @@
 use std::{error::Error, net::SocketAddr, time::Duration};
 
-use axum::{Router, http::StatusCode, routing::get};
+use axum::{
+    Router,
+    http::StatusCode,
+    routing::{get, post},
+};
 use dotenvy::dotenv;
 use renewable_ts_axum::{
     db::{establish_pg_connection, seed_database},
@@ -10,6 +14,58 @@ use renewable_ts_axum::{
 use tokio::net::TcpListener;
 use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
 use tracing::{error, info};
+
+mod routing {
+    use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+    use deadpool_diesel::postgres::Pool;
+    use diesel::{ExpressionMethods as _, QueryDsl, RunQueryDsl as _, SelectableHelper as _};
+    use renewable_ts_axum::model::{database::QueryHistory, request::TimeSeriesAggregationRequest};
+    use serde_json::json;
+    use tracing::error;
+
+    const DEFAULT_HISTORY_LIMIT: i64 = 10;
+
+    pub async fn handler_404() -> impl IntoResponse {
+        (StatusCode::NOT_FOUND, "")
+    }
+
+    pub async fn post_query_ts(
+        State(_pg_pool): State<Pool>,
+        Json(_request): Json<TimeSeriesAggregationRequest>,
+    ) -> impl IntoResponse {
+        (StatusCode::INTERNAL_SERVER_ERROR, "")
+    }
+
+    pub async fn get_query_history(State(pg_pool): State<Pool>) -> impl IntoResponse {
+        use renewable_ts_axum::renewable_schema::query_history::dsl::{executed_at, query_history};
+
+        let Ok(conn) = pg_pool.get().await else {
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Internal Error").into_response();
+        };
+
+        let history_result = conn
+            .interact(|conn| {
+                query_history
+                    .select(QueryHistory::as_select())
+                    .order_by(executed_at.desc())
+                    .limit(DEFAULT_HISTORY_LIMIT)
+                    .get_results::<QueryHistory>(conn)
+            })
+            .await;
+
+        match history_result {
+            Ok(Ok(records)) => Json(json!(records)).into_response(),
+            Ok(Err(e)) => {
+                error!("Error executing Query History: {e}");
+                (StatusCode::INTERNAL_SERVER_ERROR, "Internal Error").into_response()
+            }
+            Err(e) => {
+                error!("Error executing Query History: {e}");
+                (StatusCode::INTERNAL_SERVER_ERROR, "Internal Error").into_response()
+            }
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -23,9 +79,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     seed_database(&pg_pool).await?;
 
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        // TODO: .route()  - History Endpoint
-        // TODO: .route()  - Query Endpoint
+        // Query Endpoint
+        .route("/timeseries/v1/query", post(routing::post_query_ts))
+        // Query History Endpoint
+        .route(
+            "/timeseries/v1/query/history",
+            get(routing::get_query_history),
+        )
+        .fallback(routing::handler_404)
         .layer((
             TraceLayer::new_for_http(),
             TimeoutLayer::with_status_code(StatusCode::GATEWAY_TIMEOUT, Duration::from_secs(10)),
